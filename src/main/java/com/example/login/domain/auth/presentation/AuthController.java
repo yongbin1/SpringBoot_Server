31ca8c85f0e4a2aa77a0d4eadc38095c030b9ab0
yongbin <rhdydqls1024@naver.com>
2022-09-26 12:13:44 +0900

    :memo: :: Swagger 문서

--- src/main/java/com/example/login/domain/auth/presentation/AuthController.java
+++ src/main/java/com/example/login/domain/auth/presentation/AuthController.java
@@ -3,10 +3,13 @@ package com.example.login.domain.auth.presentation;
 import com.example.login.domain.auth.presentation.dto.UserSignInRequest;
 import com.example.login.domain.auth.presentation.dto.UserSignUpRequest;
 import com.example.login.domain.auth.service.AuthService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "유저")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -14,6 +17,7 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @ApiOperation("회원가입을 합니다")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sign-up")
     public String userSignUp(
@@ -22,6 +26,7 @@ public class AuthController {
         return authService.signUp(request);
     }
 
+    @ApiOperation("로그인을 합니다")
     @PostMapping("/sign-in")
     public String userSignIn(
             @RequestBody UserSignInRequest request
--- src/main/java/com/example/login/domain/post/presentation/PostController.java
+++ src/main/java/com/example/login/domain/post/presentation/PostController.java
@@ -5,10 +5,13 @@ import com.example.login.domain.post.presentation.dto.ModifyPostRequest;
 import com.example.login.domain.post.presentation.dto.response.PostListResponse;
 import com.example.login.domain.post.presentation.dto.response.PostResponse;
 import com.example.login.domain.post.service.PostService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "게시글")
 @RestController
 @RequestMapping("/post")
 @RequiredArgsConstructor
@@ -16,6 +19,7 @@ public class PostController {
 
     private final PostService postService;
 
+    @ApiOperation("게시글을 작성합니다")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{user-id}")
     public Long createPost(
@@ -25,6 +29,7 @@ public class PostController {
         return postService.createPost(userId, request);
     }
 
+    @ApiOperation("게시글을 수정합니다")
     @PatchMapping("/{post-id}")
     public Long modifyPost(
             @PathVariable("post-id") Long postId,
@@ -33,6 +38,7 @@ public class PostController {
         return postService.modifyPost(postId, request);
     }
 
+    @ApiOperation("게시글을 삭제합니다")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{post-id}")
     public void deletePost(
@@ -41,6 +47,7 @@ public class PostController {
         postService.deletePost(postId);
     }
 
+    @ApiOperation("게시글 정보를 불러옵니다")
     @GetMapping("/{post-id}")
     public PostResponse getPost(
             @PathVariable("post-id") Long postId
@@ -48,6 +55,7 @@ public class PostController {
         return postService.getPost(postId);
     }
 
+    @ApiOperation("게시글 목록을 불러옵니다")
     @GetMapping("/")
     public PostListResponse getPostList() {
         return postService.getPostList();
